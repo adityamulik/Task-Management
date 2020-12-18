887fd6be61ef9afb4afb9402682a73b9b311cd05
aditya <aditya.mulik@gmail.com>
2020-12-18 00:31:32 -0500

    Debugging

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,10 +25,15 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
 # Static Files
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WHITENOISE_USE_FINDERS = True
