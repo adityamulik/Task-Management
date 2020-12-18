bcefcef59b4b84d4810b94e3b23abcb03315a4b9
aditya <aditya.mulik@gmail.com>
2020-12-17 22:49:36 -0500

    Updated secret

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,17 +6,7 @@ from .base import *
 
 DEBUG = False
 
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
-
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
-
-SECRET_KEY = get_secret('SECRET_KEY')
+SECRET_KEY = '&**mifsw3@hi_1an=!o76li#^av!#z2f3k4=5!9qohf%$)n6ke'
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
