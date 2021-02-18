7f90b63020625e5703ab8967eb58647fd7b9bb5a
adityamulik <aditya.mulik@gmail.com>
2021-02-18 19:55:03 +0530

    Models Changes & Validations

--- taskmanagement/admin.py
+++ taskmanagement/admin.py
@@ -3,9 +3,9 @@ from .models import Project, Task
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['title', 'duration']
+    list_display = ['title', 'start_date', 'due_date']
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['title', 'project', 'start_date', 'end_date']
+    list_display = ['title', 'project', 'start_date', 'due_date']
--- taskmanagement/models.py
+++ taskmanagement/models.py
@@ -1,29 +1,55 @@
 import os
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.conf import settings
 from django.db import models
 
-def images_path():
-    return os.path.join(settings.LOCAL_FILE_DIR, 'images')
 
-class Project(models.Model):
+class Base(models.Model):
+    """
+    Base Abstract Class created with common fields
+    in both Project & Task Models.
+    """
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    due_date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+    def clean(self, *args, **kwargs):
+        if self.due_date < self.start_date:
+            raise ValidationError("Date cannot be smaller than start date.")
+        else:
+            super().save(*args, **kwargs)
+
+
+class Project(Base):
     """
     Each individual projects consisting listed
     tasks opened by individual users.
-    """
+    """    
+
+    STATUS = [
+        ('A', 'Active'),
+        ('IA' ,'Inactive'),
+    ]
+
+    project_id = models.AutoField(primary_key=True,)
     owner = models.ForeignKey(User,
                               related_name='project_created',
                               on_delete=models.PROTECT)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    duration = models.DurationField()
-    image = models.ImageField(upload_to='project_images/', blank=True) 
+    status = models.CharField(max_length=2, choices=STATUS)
+
+    class Meta:
+        db_table = "projects"
 
     def __str__(self):
         return self.title
 
 
-class Task(models.Model):
+class Task(Base):
     """
     Each task opened by individual user in
     a project.
@@ -35,12 +61,29 @@ class Task(models.Model):
         ('C', 'Closed'),
     ]
 
-    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
-    title = models.CharField(max_length=250)
-    description = models.TextField()
+    LABEL = [
+        ('H', 'High Priority'),
+        ('L', 'Low Priority'),
+        ('M', 'Medium Priority'),
+    ]
+
+    task_id = models.AutoField(primary_key=True)
+    assignee = models.ForeignKey(User,
+                              related_name='task_created',
+                              on_delete=models.PROTECT)
+    project = models.ForeignKey(Project, 
+                                related_name="tasks", 
+                                on_delete=models.CASCADE, 
+                                verbose_name="Project Associated: ")  
     status = models.CharField(max_length=2, choices=STATUS)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    labels = models.CharField(max_length=10, choices=LABEL)
+
+    class Meta:
+        db_table = "tasks"
 
     def __str__(self):
         return self.title
+
+
+class Workflow(models.Model):
+    pass
